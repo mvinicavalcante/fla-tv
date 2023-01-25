c950760df0e840646d2b2f6260e4966ed7a79b86
Marcos Vinícius Cavalcante Oliveira <mvinic2011@gmail.com>
2023-01-26 00:14:36 +0044

    Iniciando home

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,8 +1,10 @@
 import './styles.scss'
+import BannerImg from '../../assets/jogo-aovivo.svg' 
 
 const Banner = () => {
     return(
         <div className="banner">
+            <img className='svg' src={BannerImg}/>
         </div>
     )
 }
