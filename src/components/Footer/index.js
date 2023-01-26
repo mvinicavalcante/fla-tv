18c58944b43f3a9b8a3cd0ae5d3d251bc7727be0
Marcos Vinícius Cavalcante Oliveira <mvinic2011@gmail.com>
2023-01-26 22:51:35 +0044

    Banner responsivo(pode mudar)

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,8 @@
 import './styles.scss'
 
-import parceiroMaster from '../../assets/parceiro-master.svg'
-
 const Footer = () => {
     return(
         <div className="footer">
-            <img src={parceiroMaster} />
             <div className='footer-rodape'>
                 <p>Desenvolvido por <strong>Marcos Vinícius</strong> ©</p>
             </div>
